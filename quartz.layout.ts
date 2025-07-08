101e1825154167f95b062393a3dfe49d73741c8b
Jay Stothard <jay.stothard@krakenflex.com>
2025-07-08 16:40:17 +0100

    Quartz sync: Jul 8, 2025, 4:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      filterFn: (page) => page.slug.includes("copilot-custom-prompts"),
+    }),
   ],
   right: [
     Component.Graph(),
